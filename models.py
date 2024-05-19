66b0bf7f92f31dc2d76deacb314e1ae892425d14
Ivan Timofeev <timofeevis89@gmail.com>
2024-05-19 14:12:05 +0300

    additional task done

--- models.py
+++ models.py
@@ -1,19 +1,18 @@
 import atexit
+import datetime
 import os
 
-import datetime
-
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Integer, String, DateTime, func, Text, ForeignKey
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text, create_engine, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 
 PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
@@ -28,39 +27,47 @@ class Base(DeclarativeBase):
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False,
-                                      unique=True, index=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
     password: Mapped[str] = mapped_column(String(100), nullable=False)
-    registration_time: Mapped[datetime.datetime] = mapped_column(DateTime,
-                                                        server_default=func.now())
+    email: Mapped[str] = mapped_column(
+        String(100), nullable=False, index=True, unique=True
+    )
+    registration_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime, server_default=func.now()
+    )
 
     @property
     def dict(self):
         return {
-            'id': self.id,
-            'name': self.name,
-            'registration_time': self.registration_time.isoformat(),
+            "id": self.id,
+            "name": self.name,
+            "registration_time": self.registration_time.isoformat(),
         }
 
+
 class Ad(Base):
-    __tablename__ = 'ads'
+    __tablename__ = "ads"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     header: Mapped[str] = mapped_column(String(200), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
-    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
-    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, server_default=func.now()
+    )
+    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
 
     @property
     def dict(self):
         return {
-            'id': self.id,
-            'header': self.header,
-            'description': self.description,
-            'created_at': self.created_at.isoformat(),
+            "id": self.id,
+            "header": self.header,
+            "description": self.description,
+            "created_at": self.created_at.isoformat(),
         }
 
+
+# Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
--- server.py
+++ server.py
@@ -1,10 +1,17 @@
+from typing import Union
+
 import flask
-from flask import request, jsonify
+from flask import jsonify, request
 from flask.views import MethodView
+from flask_bcrypt import Bcrypt
+from sqlalchemy import select
 
-from models import User, Ad, Session
+from errors import AuthenticationError, HttpError
+from models import Ad, Session, User
 
-app = flask.Flask('app')
+app = flask.Flask("app")
+bcrypt = Bcrypt(app)
+
 
 @app.before_request
 def before_request():
@@ -17,22 +24,67 @@ def after_request(response: flask.Response):
     request.session.close()
     return response
 
+
 def add_db_obj(obj):
     request.session.add(obj)
     request.session.commit()
     return obj
 
-def get_add(ad_id:int):
+
+@app.errorhandler(HttpError)
+@app.errorhandler(AuthenticationError)
+def error_handler(err: Union[HttpError, AuthenticationError]):
+    json_response = jsonify({"status": "error", "message": err.message})
+    json_response.status_code = err.status_code
+    return json_response
+
+
+def get_add(ad_id: int):
     ad = request.session.query(Ad).get(ad_id)
+    if ad is None:
+        raise HttpError(status_code=404, message="Ad doesn't exist")
     return ad
+
+
+def hash_password(password: str) -> str:
+    password = password.encode()
+    password = bcrypt.generate_password_hash(password)
+    password = password.decode()
+    return password
+
+
+def check_password(hashed_password: str, password: str) -> bool:
+    password = password.encode()
+    hashed_password = hashed_password.encode()
+    return bcrypt.check_password_hash(hashed_password, password)
+
+
+def is_authorizated_user(request: flask.Request):
+    headers = request.headers
+    user_email = headers.get("email")
+    user_password = headers.get("password")
+    user = request.session.scalars(select(User).where(User.email == user_email)).first()
+    if user and check_password(user.password, user_password):
+        return user.id
+    raise AuthenticationError(
+        status_code=404, message="User not found or invalid email/password"
+    )
+
+
+def is_owner(ad, user_id: int):
+    if ad.owner_id != user_id:
+        raise AuthenticationError(status_code=409, message="Only owner can remove ad")
+
 
 class UserView(MethodView):
 
     def post(self):
         user_data = request.json
+        user_data["password"] = hash_password(user_data["password"])
         user = User(**user_data)
         add_db_obj(user)
         return jsonify(user.dict)
+
 
 class AdView(MethodView):
 
@@ -41,25 +93,28 @@ class AdView(MethodView):
         return jsonify(ad.dict)
 
     def post(self):
-        ad_data = request.json
-        ad = Ad(**ad_data)
-        add_db_obj(ad)
-        return jsonify(ad.dict)
+        user_id = is_authorizated_user(request)
+        if user_id:
+            ad_data = request.json
+            ad_data["owner_id"] = user_id
+            ad = Ad(**ad_data)
+            add_db_obj(ad)
+            return jsonify(ad.dict)
 
     def delete(self, ad_id: int):
         ad = get_add(ad_id)
+        user_id = is_authorizated_user(request)
+        is_owner(ad, user_id)
         request.session.delete(ad)
         request.session.commit()
         return jsonify({"status": "deleted"})
 
-user_view = UserView.as_view('user')
-ad_view = AdView.as_view('ad')
+
+user_view = UserView.as_view("user")
+ad_view = AdView.as_view("ad")
 
 app.add_url_rule("/user/", methods=["POST"], view_func=user_view)
 app.add_url_rule("/ads/", methods=["POST"], view_func=ad_view)
 app.add_url_rule("/ads/<int:ad_id>/", methods=["GET", "DELETE"], view_func=ad_view)
 
-
 app.run(port=8080)
-
-
